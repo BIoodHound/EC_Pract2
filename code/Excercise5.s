40c5cf997291cf4ce61ae8a988943e2056b798a0
BIoodHound <goncalves_leonardo@yahoo.com>
2017-10-25 10:15:06 +0100

    Update Excercise5.s

--- code/Excercise5.s
+++ code/Excercise5.s
@@ -1,7 +1,7 @@
 .global _start
 
 start:
-    movia r2, 0x10000020 #Initialize r2 the hex7-4
+    movia r2, 0x10000020 #Initialize r2 the hex3-0
     movia r3, 0x10000050 #Initialize r3 with the push keys
     addi r4, r0, 0x3F #Initializes the register r3 to the hex value of 0x3F which will display a cero
     stwio r4, 0(r2) #Initializes the mapped register on the address that signals r2 with the pattern of bits registered on the register r3
