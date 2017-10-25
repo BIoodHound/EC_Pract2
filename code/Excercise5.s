7d4b4aae450d9b694e5659e7614df79ff82a6878
BIoodHound <goncalves_leonardo@yahoo.com>
2017-10-25 09:49:26 +0100

    Update Excercise5.s

--- code/Excercise5.s
+++ code/Excercise5.s
@@ -5,8 +5,6 @@ start:
     movia r3, 0x10000050 #Initialize r3 with the push keys
     addi r4, r0, 0x3F #Initializes the register r3 to the hex value of 0x3F which will display a cero
     stwio r4, 0(r2) #Initializes the mapped register on the address that signals r2 with the pattern of bits registered on the register r3
-display:
-    
     
 wait:
     ldwio r5, 0(r3)
